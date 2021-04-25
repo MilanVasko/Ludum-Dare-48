33932844b4e8235ef5c1243b8ebb7143fb70161d
Milan Vaško <m.vasko2@gmail.com>
2021-04-25 20:07:36 +0200

    Don't spawn two corridors with hole after each other

--- Assets/Scripts/Corridor.cs
+++ Assets/Scripts/Corridor.cs
@@ -4,4 +4,15 @@ public class Corridor : MonoBehaviour, ISelfDestructor {
 	public void SelfDestruct() {
 		Destroy(gameObject);
 	}
+
+	public bool isCorridorWithHole {
+		get {
+			foreach (Transform child in transform) {
+				if (child.name == "CorridorWithHole") {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
 }
--- Assets/Scripts/CorridorSpawner.cs
+++ Assets/Scripts/CorridorSpawner.cs
@@ -12,6 +12,8 @@ public class CorridorSpawner : MonoBehaviour {
     public PlayerCharacter playerCharacter;
     public UnityEvent<CorridorSpawner, int, int> onNextCorridorReached;
 
+    Corridor lastSpawnedCorridor = null;
+
     int previousCorridorID;
 
     Vector3 firstSpawnLocation = Vector3.back * 2 * CORRIDOR_LENGTH;
@@ -37,12 +39,18 @@ public class CorridorSpawner : MonoBehaviour {
 	}
 
     public void OnNextCorridorReached() {
-        Corridor whichCorridor = (Random.Range(0.0f, 1.0f) < corridorWithHoleSpawnChance) ? corridorWithHole : corridor;
+        Corridor whichCorridor;
+        if (lastSpawnedCorridor != null && lastSpawnedCorridor.isCorridorWithHole) {
+            whichCorridor = corridor; // don't spawn two holes after each other, it becomes too difficult to jump over them
+        } else {
+            whichCorridor = (Random.Range(0.0f, 1.0f) < corridorWithHoleSpawnChance) ? corridorWithHole : corridor;
+        }
         SpawnCorridor(whichCorridor, ++spawnedCorridorsCount);
     }
 
     Corridor SpawnCorridor(Corridor corridor, int id) {
-        return Instantiate(corridor, CalculateCorridorPosition(id), Quaternion.identity);
+        lastSpawnedCorridor = Instantiate(corridor, CalculateCorridorPosition(id), Quaternion.identity);
+        return lastSpawnedCorridor;
     }
 
     public int CalculateCurrentPlayerCorridorID() {
